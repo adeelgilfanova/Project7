3a4c0bfaaf6d83749ad3e9586deb51730f8d422a
AdelyaGI <112945520+adeelgilfanova@users.noreply.github.com>
2022-11-04 19:42:03 +0300

    all

---
+++ Project7/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project7
+{
+    enum AccountType
+    {
+        Current,
+        Savings
+    }
+    class Bank
+    {
+        public Bank(string accountNumber, double balans, AccountType type)
+        {
+            AccountNumber = accountNumber;
+            Balans = balans;
+            Type = type;
+        }
+        public string AccountNumber { get; set; }
+        public double Balans { get; set; }
+        public AccountType Type { get; set; }
+        public override string ToString() => $"Номер счета - {AccountNumber}, Баланс - {Balans}, Тип банковского счета - {Type}";
+
+        public double PutOnAccount(double Balans)
+        {
+            Console.Write("Введите сумму для пополнения ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            return Balans + temp;
+        }
+        public double WithdrawFromAccount(double Balans)
+        {
+            Console.Write("Введите сумму для снятия ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            if (Balans >= temp)
+            {
+                return Balans - temp;
+            }
+            else
+            {
+                Console.WriteLine("На вашем счёте недостаточно средств!");
+                return Balans;
+            }
+        }
+        public void moneytransfer(Bank Bank, int amount)
+        {
+            this.Balans -= amount;
+            Bank.Balans += amount;
+        }
+
+    }
+    public static class StringExtension
+    {
+        public static string Reverse(this string str)
+        {
+            string res = string.Empty;
+            for (int i = str.Length - 1; i >= 0; --i)
+                res += str[i];
+            return res;
+        }
+    }
+    public class Temperature : IFormattable
+    {
+        private decimal temp;
+
+        public Temperature(decimal temperature)
+        {
+            if (temperature < -273.15m)
+                throw new ArgumentOutOfRangeException(String.Format("{0} is less than absolute zero.",
+                                                    temperature));
+            this.temp = temperature;
+        }
+
+        public string ToString(string format, IFormatProvider provider)
+        {
+            return temp.ToString("F2", provider) + " °C";
+        }
+    }
+
+    class Program
+    {
+        static void checkArgImplementInterface(Temperature t)
+        {
+            IFormattable form, form2;
+
+            if (t is IFormattable)
+            {
+                form = (IFormattable)t;
+            }
+            else
+            {
+                form = null;
+            }
+
+            if (form is null)
+            {
+                Console.WriteLine("Не реализует IFormattable");
+            }
+            else
+            {
+                Console.WriteLine("Реализует IFormattable");
+            }
+
+            form2 = t as IFormattable;
+
+            if (form2 is null)
+            {
+                Console.WriteLine("Не реализует IFormattable");
+            }
+            else
+            {
+                Console.WriteLine("Реализует IFormattable");
+            }
+        }
+        static void Main()
+        {
+            //Console.WriteLine("Упражнение8.1");
+            //AccountType accountType = AccountType.Current;
+            //Bank bank1 = new Bank("764HGS78", 1000, AccountType.Savings);
+            //Bank bank2 = new Bank("386vfb3", 2000, AccountType.Current);
+            //Console.WriteLine("Какую сумму хотите перевести из первого банка во второй?");
+            //int monney = Convert.ToInt32(Console.ReadLine());
+            //bank1.moneytransfer(bank2, monney);
+            //Console.WriteLine(bank1);
+            //Console.WriteLine(bank2);
+            //Console.ReadKey();
+
+            //Console.WriteLine("\nУпражнение8.2");
+            //string s = "фбвгдежзимнклн";
+            //Console.WriteLine($"Изначальная строка: {s}\nПеревернутая: {s.Reverse()}");
+
+            //Console.WriteLine("\nУпражнение8.3");
+            //const string outputFileName = "ResultText.out";
+            //string inputFileName = string.Empty;
+
+            //Console.WriteLine("Введите название входного файла: ");
+            //inputFileName = Console.ReadLine();
+
+            //if (File.Exists(inputFileName))
+            //{
+            //    File.WriteAllText(outputFileName, File.ReadAllText(inputFileName, Encoding.UTF8).ToUpper(), Encoding.UTF8);
+            //    Console.WriteLine("Результат успешно записан в файл с именем \"{0}\"", outputFileName);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Файл с именем \"{0}\" не найден!", inputFileName);
+            //}
+
+            //Console.ReadKey();
+
+            Console.WriteLine("Упражнение8.4");
+            Temperature t = new Temperature(50);
+            checkArgImplementInterface(t);
+        }
+    }
+
+}
+
---
+++ Zadachi/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadachi
+{
+    //главный
+    public class Борис
+    {
+
+    }
+    //финансовый директор(Рашид)
+    class Рашид : Борис
+    {
+        
+    }
+    class Лукас : Рашид
+    {
+
+    }
+    class Бухгалтерия : Лукас
+    {
+
+    }
+    // директор по автоматизации:
+    class Оильхам : Борис
+    {
+        
+    }
+    class Оркадий : Оильхам
+    {
+
+    }
+    class Володя : Оркадий
+    {
+
+    }
+    // системщики:
+    class Ильшат : Володя
+    {
+        
+    }
+    class Иваныч : Ильшат
+    {
+
+    }
+    class Илья : Иваныч
+    {
+
+    }
+    class Витя : Иваныч
+    {
+
+    }
+    class Женя : Иваныч
+    {
+
+    }
+    // разработчики:
+    class Сергей : Володя
+    {
+        
+    }
+    class Ляйсан : Сергей
+    {
+
+    }
+    class Марат : Ляйсан
+    {
+
+    }
+    class Дина : Ляйсан
+    {
+
+    }
+    class Ильдар : Ляйсан
+    {
+
+    }
+    class Антон : Ляйсан
+    {
+
+    }
+    internal class Program
+    {
+        static void Otvet(Type a, Type b)
+        {
+            if (a.IsSubclassOf(b)) { Console.WriteLine("Yes"); } // проверяем является ли классы наследниками друг друга
+            else { Console.WriteLine("No"); }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кто дал задачу:");
+            string A1 = Console.ReadLine();
+            Console.WriteLine("Введите кому хотите дать задачу:");
+            string B1 = Console.ReadLine();
+            Console.WriteLine("Введите название задачи:");
+            string C1 = Console.ReadLine();
+            Type A = Type.GetType("Zadachi" + "." + A1, false, true); // получаем типы классов по имени(строке)
+            Type B = Type.GetType("Zadachi" + "." + B1, false, true);
+            Console.WriteLine($"Задача {C1} от {A1} передана для {B1}.");
+            Console.WriteLine("Ответ сотрудника:");
+            Otvet(B, A);
+        }
+    }
+}
+
